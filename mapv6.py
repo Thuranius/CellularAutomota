c6512ce7add5d7e22aa22712fddd19a28550a0b7
Corey <corey.gatter@gmail.com>
2021-06-04 13:16:45 -0700

    Updating the format.
    We are not savages

--- mapv6.py
+++ mapv6.py
@@ -9,7 +9,7 @@ main = [] # Array to contain map values
 # :: Changeable values to alter the outcome ::
 mNum = 0 # This is the water height. Must be between -1 and 1
 gradient = False
-sizeX, sizeY = 960,540 # Final pixel sizes
+sizeX, sizeY = 960, 540 # Final pixel sizes
 
 # :: colors ::
 _LIGHTBLUE = (27, 133, 191)
@@ -20,7 +20,7 @@ _DARKGREEN = (67, 176, 55)
 _BLUEDIFF = tuple(map(lambda i, j: i - j, _LIGHTBLUE, _DARKBLUE))
 _GREENDIFF = tuple(map(lambda i, j: i - j, _LIGHTGREEN, _DARKGREEN))
 
-def perlinPass(octaves,x,y, lac =1 , persistence=1):
+def perlinPass(octaves, x, y, lac=1):
     """
     Taking in the x and y coordinates and produces the value after cycling through the all of the octaves.
 
@@ -35,9 +35,9 @@ def perlinPass(octaves,x,y, lac =1 , persistence=1):
     total = 0
     for i in range(octaves):
         freq *= lac
-        total += n.noise2(x*freq-freq,y*freq-freq)
+        total += n.noise2(x * freq - freq, y * freq - freq)
         max += amp
-    final = total/max
+    final = total / max
     return final
 
 def fullMap(map,sizeX,sizeY):
@@ -52,10 +52,10 @@ def fullMap(map,sizeX,sizeY):
     for x in range(sizeX):
         map.append([])
         for y in range(sizeY):
-            noise_val = perlinPass(3,x/(maNum1),y/(maNum1),1.3)
-            noise_val += .5 * perlinPass(6,x/(maNum2),y/(maNum2),1.4)
-            noise_val += .25 * perlinPass(12,x/(maNum3),y/(maNum3),1.5)
-            noise_val += .125 * perlinPass(24,x/(maNum4),y/(maNum4),1.6)
+            noise_val = perlinPass(3, x/(maNum1), y/(maNum1), 1.3)
+            noise_val += .5 * perlinPass(6, x/(maNum2), y/(maNum2), 1.4)
+            noise_val += .25 * perlinPass(12, x/(maNum3), y/(maNum3), 1.5)
+            noise_val += .125 * perlinPass(24, x/(maNum4), y/(maNum4), 1.6)
             map[x].append(noise_val)
     return map
 
@@ -72,7 +72,7 @@ def assignColor(map, sizeX, sizeY):
     return map
 
 def assignGradient(Omap, sizeX, sizeY):
-    global mNum, _GREENDIFF,_LIGHTGREEN,_BLUEDIFF,_LIGHTBLUE
+    global mNum, _GREENDIFF, _LIGHTGREEN, _BLUEDIFF, _LIGHTBLUE
     for x in range(sizeX):
         for y in range(sizeY):
             gradientPercent = abs(Omap[x][y])
@@ -99,12 +99,12 @@ def detectBorder(map, sizeX, sizeY, radius = 2, random = False):
             newMap[x].append(11)
             found = False
             if map[x][y] >= mNum:
-                for i in range((rd*-1)+1,rd):
-                    for j in range((rd*-1)+1,rd):
-                        if i+x > -1 and j+y > -1:
+                for i in range((rd * -1) + 1, rd):
+                    for j in range((rd * -1) + 1, rd):
+                        if i + x > -1 and j + y > -1:
                             try:
                                 if not found:
-                                    if map[x+i][y+j] < mNum:
+                                    if map[x + i][y + j] < mNum:
                                         newMap[x][y] = 999
                                         found = True
                                     else:
@@ -117,18 +117,18 @@ def detectBorder(map, sizeX, sizeY, radius = 2, random = False):
     return newMap
 
 def newImg(map, sizeX, sizeY):
-    img = Image.new(mode = "RGB", size = (sizeX,sizeY))
+    img = Image.new(mode = "RGB", size = (sizeX, sizeY))
     pixels = img.load()
     for x in range(sizeX):
         for y in range(sizeY):
-            pixels[x,y] = map[x][y]
+            pixels[x, y] = map[x][y]
     img.show()
 
-main = fullMap(main,sizeX,sizeY)
-main = detectBorder(main,sizeX,sizeY)
+main = fullMap(main, sizeX, sizeY)
+main = detectBorder(main, sizeX, sizeY)
 if gradient:
-    main = assignGradient(main,sizeX,sizeY)
+    main = assignGradient(main, sizeX, sizeY)
 else:
-    assignColor(main,sizeX,sizeY)
+    assignColor(main, sizeX, sizeY)
 
-newImg(main, sizeX,sizeY)
+newImg(main, sizeX, sizeY)
